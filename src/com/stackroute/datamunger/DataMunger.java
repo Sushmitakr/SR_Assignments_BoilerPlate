87e3c4d2d9416b0e8879ab66298e502894ec1cf4
sushmita kumari <sushmita2607k@gmail.com>
2018-09-05 12:06:25 +0530

    final changes

---
+++ src/com/stackroute/datamunger/DataMunger.java
@@ -0,0 +1,39 @@
+package com.stackroute.datamunger;
+
+import java.util.Scanner;
+
+public class DataMunger {
+
+	public static void main(String[] args) {
+
+
+		/* read a sentence from the user */
+
+		/*
+		 * call getWordCount() method which should return no. of words present
+		 * in the string and display the same
+		 */
+
+	}
+
+	
+	/*
+	 * This method is used to calculate the no. of words present in the given
+	 * string. Please note that in a sentence, words are split by space. Please change the method return 
+	 * type to int
+	 */
+	public Object getWordCount(String string) {
+		// TODO Auto-generated method stub
+		//string = "this is a sample sentence";
+		String[] str = string.split(" ");
+		
+		if(string == "") {
+			return 0;
+		}
+		
+		
+		return str.length;
+	}
+	
+
+}
